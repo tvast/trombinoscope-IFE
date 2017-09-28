8371ba37ebe20d0f68e9c363e5aa9834928ac047
Theophile <theophile@mediateca-06.institutfrancais.es>
2017-09-28 17:02:53 +0200

    first commit

---
+++ js/anim.js
@@ -0,0 +1,11 @@
+var domAnimator = new DomAnimator();
+    domAnimator.addFrame('o_o');
+    domAnimator.addFrame('-_o');
+    domAnimator.addFrame('o_o');
+    domAnimator.addFrame('-_o');
+    domAnimator.addFrame('o_o');
+    domAnimator.addFrame('-_o');
+    domAnimator.addFrame('o_o');
+    domAnimator.addFrame('-_o');
+    domAnimator.animate();
+    domAnimator.animate(4000); //ms
