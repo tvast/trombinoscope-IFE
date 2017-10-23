61b3e7f4fcd5c6449453a2d0133048e0daa78bb6
Theophile <theophile@mediateca-06.institutfrancais.es>
2017-10-23 17:09:19 +0200

    add files

--- js/anim.js
+++ js/anim.js
@@ -1,10 +1,10 @@
 var domAnimator = new DomAnimator();
     domAnimator.addFrame('o_o');
     domAnimator.addFrame('-_o');
-    domAnimator.addFrame('o_o');
+    domAnimator.addFrame('Institut francais de madrid');
     domAnimator.addFrame('-_o');
     domAnimator.addFrame('o_o');
-    domAnimator.addFrame('-_o');
+    domAnimator.addFrame('Institut francais Espagne');
     domAnimator.addFrame('o_o');
     domAnimator.addFrame('-_o');
     domAnimator.animate();
